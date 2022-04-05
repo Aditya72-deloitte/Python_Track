e1e7c4d6396c1669ef3aba83f5a65e1f5bdc45f1
Aditya72 <adityakumar72@deloitte.com>
2022-04-05 14:51:35 +0530

    Mini_Assignment_1_Added

--- Mini_Assignment_1/main.py
+++ Mini_Assignment_1/main.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 class StringClass:
     """
     A representation of a person
@@ -11,8 +14,9 @@ class StringClass:
 
     def getStr(self):
         self.firstname = input('Enter your String : ')
+        return self.firstname
 
-    def show_full_name(self):
+    def length(self):
         return len(self.firstname)
 
     # Python3 program to Split string into characters
@@ -20,9 +24,42 @@ class StringClass:
         return [char for char in self.firstname]
 
 
+class SearchCommonElements:
+    def getStr():
+        strin2 = input('Enter 2nd String : ')
+        return strin2
+
+    def common(str1, str2):
+        # convert both strings into counter dictionary
+        dict1 = Counter(str1)
+        dict2 = Counter(str2)
+
+        # take intersection of these dictionaries
+        commonDict = dict1 & dict2
+
+        if len(commonDict) == 0:
+            print(-1)
+            return
+
+        # get a list of common elements
+        commonChars = list(commonDict.elements())
+
+        # sort list in ascending order to print resultant
+        # string on alphabetical order
+        commonChars = sorted(commonChars)
+
+        # join characters without space to produce
+        # resultant string
+        return [char for char in ''.join(commonChars)]
+
+
 
 # creating an object with the class
 person2 = StringClass()
-person2.getStr()
+str1 = person2.getStr()
 print(person2.split())
-print(person2.show_full_name())
+print(person2.length())
+
+person1 = SearchCommonElements()
+str2 = SearchCommonElements.getStr()
+print(SearchCommonElements.common(str1,str2))
